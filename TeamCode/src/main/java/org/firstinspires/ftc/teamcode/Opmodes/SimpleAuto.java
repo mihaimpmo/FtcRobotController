b9e370831a1b8de257d4909653cbad910736df59
mihai <mihaimpmo@gmail.com>
2026-04-24 20:15:35 +0300

    Auto (problem with fr)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/SimpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/SimpleAuto.java
@@ -1,12 +1,38 @@
 package org.firstinspires.ftc.teamcode.Opmodes;
 
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Subsystems.AutoActions;
+import org.firstinspires.ftc.teamcode.Subsystems.SwerveDrive;
+@Configurable
 @Autonomous(name = "LeaveAuto", group = "Auto")
-public class SimpleAuto extends Auto {
+public class SimpleAuto extends LinearOpMode {
+    public static double PINPOINT_X_OFFSET_MM = -84.0;
+    public static double PINPOINT_Y_OFFSET_MM = -168.0;
 
     @Override
-    protected void runPath() {
-        autoDrive.forward(24.0);
+    public void runOpMode() throws InterruptedException {
+        SwerveDrive swerve = new SwerveDrive(this.hardwareMap);
+        GoBildaPinpointDriver pinpoint = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
+        AutoActions actions = new AutoActions(swerve, pinpoint, this);
+        pinpoint.setOffsets(PINPOINT_X_OFFSET_MM, PINPOINT_Y_OFFSET_MM, DistanceUnit.MM);
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        pinpoint.resetPosAndIMU();
+        waitForStart();
+        boolean homingSuccess = swerve.homeAllModules(this);
+        sleep(5000);
+
+        if (!homingSuccess) {
+            telemetry.addLine("WARNING: Not all modules homed successfully!");
+            telemetry.update();
+            sleep(2000);
+        }
+
+
     }
 }
